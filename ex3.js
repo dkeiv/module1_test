9a871c80c04ce37615bcef36d4b04055a9b78fd6
dkeiv <vudangkhoi1994@gmail.com>
2024-06-04 09:27:58 +0700

    ex3

---
+++ ex3.js
@@ -0,0 +1,18 @@
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+}
+
+function findPrime(arr) {
+  const res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (isPrime(arr[i])) {
+      res.push(arr[i]);
+    }
+  }
+
+  return res.sort((a, b) => a > b);
+}
