6a67eb4f6f68ec8dab85b727fc906232764763b4
dkeiv <vudangkhoi1994@gmail.com>
2024-06-04 10:35:48 +0700

    clean up

--- ex2.js
+++ ex2.js
@@ -1,9 +1,11 @@
 function countUpperCase(str) {
   let count = 0;
+
   for (let i = 0; i < str.length; i++) {
     if ('A' <= str[i] && str[i] <= 'Z') {
       count++;
     }
   }
+  
   return count === 0 ? 'The Strings is not contain upper char' : count;
 }
